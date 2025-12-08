9cc4ef9aee494da96e4e83396d55498083e893d8
Sep29th <caulata1234@gmail.com>
2025-12-08 19:05:15 +0700

    feat: add directUrl to datasource configuration in Prisma setup
    - Updated the Prisma configuration to include a directUrl for the datasource, allowing for more flexible database connection options.
    - Adjusted formatting for improved readability in both prisma.config.ts and schema.prisma files.

--- prisma.config.ts
+++ prisma.config.ts
@@ -3,14 +3,17 @@ import { defineConfig } from "prisma/config";
 import path from "path";
 
 export default defineConfig({
-  schema: path.join(__dirname, "prisma", "schema.prisma"),
-  migrations: {
-    path: path.join(__dirname, "prisma", "migrations"),
-    seed: 'ts-node --compiler-options {"module":"CommonJS"} prisma/seed.ts',
-  },
-  datasource: {
-    url:
-      process.env.DATABASE_URL ??
-      "postgresql://yasoco:yasoco@localhost:5432/yasoco?schema=public",
-  },
+	schema: path.join(__dirname, "prisma", "schema.prisma"),
+	migrations: {
+		path: path.join(__dirname, "prisma", "migrations"),
+		seed: 'ts-node --compiler-options {"module":"CommonJS"} prisma/seed.ts',
+	},
+	datasource: {
+		url:
+			process.env.DATABASE_URL ??
+			"postgresql://yasoco:yasoco@localhost:5432/yasoco?schema=public",
+		directUrl:
+			process.env.DIRECT_URL ??
+			"postgresql://yasoco:yasoco@localhost:5432/yasoco?schema=public",
+	},
 });
